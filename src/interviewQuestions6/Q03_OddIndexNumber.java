79909480f3f249f4a279085d85aa3585dca30ed3
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-04-18 15:36:09 +0300

    18.04.22

---
+++ src/interviewQuestions6/Q03_OddIndexNumber.java
@@ -0,0 +1,8 @@
+package interviewQuestions6;
+
+
+public class Q03_OddIndexNumber {
+    // indexi tek sayi olan karakterleri yazdiran bir code create ediniz
+
+
+}
---
+++ src/interviewQuestions6/Q06_DuplicateValue.java
@@ -0,0 +1,15 @@
+package interviewQuestions6;
+
+
+public class Q06_DuplicateValue {
+       /*
+        Write a code that returns the duplicate chars in a String array.
+        (interview Question)
+        Input :
+        String str=“Javaisalsoeasy”
+        Output: [a, s]
+         */
+
+
+}
+
